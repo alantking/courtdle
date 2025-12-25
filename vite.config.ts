5145612249ee075dc4b176837faa579a5e0f904e
alantking <58782818+alantking@users.noreply.github.com>
2025-12-25 03:29:44 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/courtdle/',  
-  plugins: [vue()],
+  base: '/courtdle/',  // <-- Add this for GitHub Pages repo deployment
+  plugins: [
+    vue({
+      // Enable Vue 3 Reactivity Transform to use $ref, $computed, $fromRefs
+      reactivityTransform: true
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        // Optional: tweak if you need to change chunk naming
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
